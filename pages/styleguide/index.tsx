ff305001167aba2bb8dad38ae1379cfa9c4b0bbe
Kelly Packer <kelly@kellypacker.com>
2022-01-18 13:49:31 -0700

    More link animations

--- pages/styleguide/index.tsx
+++ pages/styleguide/index.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 
 const StyleGuidePage = () => (
     <div className="container mx-auto py-10">
-        <h2 className="h5 overline">Typography</h2>
+        <h2 className="h5 overline text-center">Typography</h2>
+        <hr className="my-8" />
+        <h2 className="h5 overline mt-8">h1</h2>
         <h1 className="h1">We’re glad to be here, working with you.</h1>
+        <h2 className="h5 overline mt-8">h2</h2>
         <h2 className="h2">We’re glad to be here, working with you.</h2>
+        <h2 className="h5 overline mt-8">h3</h2>
         <h3 className="h3">We’re glad to be here, working with you.</h3>
+        <h2 className="h5 overline mt-8">h4</h2>
         <h4 className="h4">We’re glad to be here, working with you.</h4>
+        <h2 className="h5 overline mt-8">h5</h2>
         <h5 className="h5">We’re glad to be here, working with you.</h5>
+
+        <h5 className="h5 overline mt-8">Overline</h5>
+        <h5 className="h5 overline">Solutions</h5>
+        <h2 className="h5 overline mt-8">Paragraph</h2>
+        <p className="mb-6">
+            Our work is both high-tech and personal. We apply our knowledge of complex digital solutions to help real
+            people solve real problems. From smart design to strategic programming,​ we employ technology as a medium
+            for advancing your business and making human connections.
+        </p>
+
+        <h2 className="h5 overline mt-8">Paragraph extra-large</h2>
+        <p className="p-xl">
+            Our work is both high-tech and personal. We apply our knowledge of complex digital solutions to help real
+            people solve real problems. From smart design to strategic programming,​ we employ technology as a medium
+            for advancing your business and making human connections.
+        </p>
 
         <hr className="my-8" />
         <h2 className="h5 overline">Links</h2>
@@ -15,7 +37,13 @@ const StyleGuidePage = () => (
             Arts &amp; Culture
         </a>
         <br />
-        <a href="#">Read More</a>
+        <a href="#" className="border-b-in h4">
+            Arts &amp; Culture
+        </a>
+        <br />
+        <a href="#" className="emdash-in h5">
+            Read More
+        </a>
     </div>
 );
 
