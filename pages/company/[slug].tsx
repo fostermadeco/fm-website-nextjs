ae13fa4d4af0911b1f7e4991d8ca69144b10caff
Kelly Packer <kelly@kellypacker.com>
2022-01-28 16:14:22 -0700

    Aliases working in eslint

--- pages/company/[slug].tsx
+++ pages/company/[slug].tsx
@@ -1,10 +1,10 @@
 import { GetServerSideProps } from 'next';
 import React from 'react';
-import { TypePage, TypePageCompany, TypePageJob } from '../../lib/types';
-import MainNav from '../../components/MainNav';
-import { PageHead } from '../../components/PageHead';
-import PageIntroDetail from '../../components/renderer/PageIntroDetail';
-import BlockRenderer from '../../components/renderer/BlockRenderer';
+import { TypePage, TypePageCompany, TypePageJob } from '@types';
+import { PageHead } from '@components/PageHead';
+import MainNav from '@components/MainNav';
+import PageIntroDetail from '@components/renderer/PageIntroDetail';
+import BlockRenderer from '@components/renderer/BlockRenderer';
 import { getPage, getPages } from '../../api/api';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
