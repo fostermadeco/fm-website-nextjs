689f1e70305fa5e9d7cfa7b056fa5c2b97894bdc
Bryan Sylla <bryansylla@Bryans-MBP.attlocal.net>
2022-03-14 12:37:40 -0500

    added images to case studies

--- components/renderer/FeaturedCaseStudies.tsx
+++ components/renderer/FeaturedCaseStudies.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { TypeBlockFeaturedCaseStudiesFields, TypeCaseStudyFields, TypePageCaseStudy, TypePageCaseStudyFields } from '@types';
+import Image from 'next/image';
+import { TypeBlockFeaturedCaseStudiesFields } from '@types';
 
 const FeaturedCaseStudies = ({ fields }: { fields: TypeBlockFeaturedCaseStudiesFields }) => {
     if (!fields) return null;
@@ -7,18 +8,19 @@ const FeaturedCaseStudies = ({ fields }: { fields: TypeBlockFeaturedCaseStudiesF
     const { overline, headerText, button, caseStudies } = fields;
     return <div>
         <div>
-
+            {overline}
         </div>
         <div>
-
+            {headerText}
         </div>
         <div>
             {caseStudies?.map((pageCaseStudy) => {
                 const title = pageCaseStudy.fields.title
                 const caseStudyFields = pageCaseStudy.fields.content.fields.caseStudy.fields
-                const {summary, heroImage, } = caseStudyFields
+                const { summary, heroImage, } = caseStudyFields
                 console.log(heroImage)
-                return (<div className="grid grid-cols-6">
+                console.log(heroImage.fields.media.fields.file.url)
+                return (<div className="grid grid-cols-2">
                     <div >
                         <h5>
                             /Case Study
@@ -33,7 +35,11 @@ const FeaturedCaseStudies = ({ fields }: { fields: TypeBlockFeaturedCaseStudiesF
 
                     </div>
                     <div>
-                        
+                        <Image
+                            src={`https:${heroImage.fields.media.fields.file.url}`}
+                            width={heroImage.fields.media.fields.file.details.image.width}
+                            height={heroImage.fields.media.fields.file.details.image.height}
+                        />
                     </div>
 
                 </div>)
