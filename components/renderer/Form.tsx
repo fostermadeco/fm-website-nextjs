62c7f169ed2ad3ad3eab4fc7489c47eeea90af16
Bryan Sylla <bryansylla@Bryans-MBP.attlocal.net>
2022-03-16 13:58:45 -0500

    add CtaNewsletter form

--- components/renderer/Form.tsx
+++ components/renderer/Form.tsx
@@ -2,15 +2,11 @@ import FormApply from '@components/FormApply';
 import FormContact from '@components/FormContact';
 import { TypeFormFields } from '@types';
 
-const Form = ({ fields }: { fields: TypeFormFields }) => {
-    const { intro } = fields;
-
-    return (
-        <>
-            {fields.id === 'apply' && <FormApply form={fields} />}
-            {fields.id === 'contact' && <FormContact form={fields} />}
-        </>
-    );
-};
+const Form = ({ fields }: { fields: TypeFormFields }) => (
+    <>
+        {fields.id === 'apply' && <FormApply form={fields} />}
+        {fields.id === 'contact' && <FormContact form={fields} />}
+    </>
+);
 
 export default Form;
