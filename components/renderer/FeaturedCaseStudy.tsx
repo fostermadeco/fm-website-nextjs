46115809b047a3009c881b6a845d6a5f8579fb96
Kelly Packer <kelly@kellypacker.com>
2022-03-18 07:35:53 -0600

    Add fade in to featured case study images

--- components/renderer/FeaturedCaseStudy.tsx
+++ components/renderer/FeaturedCaseStudy.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LinkWrapper from '@components/LinkWrapper';
 import Image from 'next/image';
 import { TypePage, TypePageCaseStudy } from '@types';
+import AnimationFadeIn from '@components/AnimationFadeIn';
+import { useAnimation } from 'framer-motion';
 
 type FeaturedCaseStudyPropTypes = {
     pageCaseStudy: TypePage;
@@ -11,27 +13,37 @@ const FeaturedCaseStudy = ({ pageCaseStudy }: FeaturedCaseStudyPropTypes) => {
     const { title } = pageCaseStudy.fields;
     const { fields: caseStudyPageFields } = pageCaseStudy.fields.content as TypePageCaseStudy;
     const { summary, heroImage } = caseStudyPageFields.caseStudy.fields;
+    const [imageIsLoaded, setImageIsLoaded] = useState(false);
+    const animationControls = useAnimation();
+
+    useEffect(() => {
+        if (!imageIsLoaded) return;
+        animationControls.start('visible');
+    }, [imageIsLoaded]);
+
     return (
         <LinkWrapper page={pageCaseStudy}>
             <a>
-                <div className="group items-center grid-cols-1 gap-0 mx-6 md:mx-0 md:grid md:grid-cols-14 mb-24 md:gap-8">
+                <div className="items-center grid-cols-1 gap-0 mx-6 mb-24 group md:mx-0 md:grid md:grid-cols-14 md:gap-8">
                     <div className="col-span-4 col-start-2">
-                        <div className="h5 overline text-white">Case Study</div>
-                        <div className="h4 group-hover:text-lime text-white">{title}</div>
+                        <div className="text-white h5 overline">Case Study</div>
+                        <div className="text-white h4 group-hover:text-lime">{title}</div>
                         <p className="text-white">{summary}</p>
-                        <div
-                            className="inline-block mt-4 h5 emdash-in
-                                        text-white"
-                        >
-                            Read More
-                        </div>
+                        <div className="inline-block mt-4 text-white h5 emdash-in">Read More</div>
                     </div>
                     <div className="col-span-9">
-                        <Image
-                            src={`https:${heroImage.fields.media.fields.file.url}`}
-                            width={heroImage.fields.media.fields.file.details.image?.width}
-                            height={heroImage.fields.media.fields.file.details.image?.height}
-                        />
+                        <AnimationFadeIn animate={animationControls}>
+                            <div className="relative w-full h-72 md:h-[550px]">
+                                <Image
+                                    src={`https:${heroImage.fields.media.fields.file.url}`}
+                                    height={550}
+                                    layout="fill"
+                                    objectFit="cover"
+                                    objectPosition="center"
+                                    onLoadingComplete={() => setImageIsLoaded(true)}
+                                />
+                            </div>
+                        </AnimationFadeIn>
                     </div>
                 </div>
             </a>
