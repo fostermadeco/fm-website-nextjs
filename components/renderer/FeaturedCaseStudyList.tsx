d92ba435a2b098dbd45232e81af309686ac900c4
Kelly Packer <kelly@kellypacker.com>
2022-03-18 07:35:16 -0600

    Change black to be full width

--- components/renderer/FeaturedCaseStudyList.tsx
+++ components/renderer/FeaturedCaseStudyList.tsx
@@ -12,20 +12,22 @@ const FeaturedCaseStudyList = (props: FeaturedCaseStudyListPropTypes) => {
 
     const { overline, headerText, caseStudies, button } = fields;
     return (
-        <div className="container">
-            <div className="bg-black pb-40">
-                <div className={` pb-20 pt-40 text-center`}>
-                    <div className="h5 overline text-white">{overline}</div>
-                    <div className="h3 text-white">{headerText}</div>
-                </div>
-                {caseStudies?.map((pageCaseStudy) => (
-                    <FeaturedCaseStudy key={pageCaseStudy.fields.content.sys.id} pageCaseStudy={pageCaseStudy} />
-                ))}
-                {button && (
-                    <div className="flex justify-center mt-20">
-                        <ButtonCircle bgColor="ivory" button={button.fields} />
+        <div className="bg-black">
+            <div className="container">
+                <div className="pb-40">
+                    <div className="max-w-2xl pt-40 pb-20 mx-auto text-center">
+                        <div className="text-white h5 overline">{overline}</div>
+                        <div className="text-white h3">{headerText}</div>
                     </div>
-                )}
+                    {caseStudies?.map((pageCaseStudy) => (
+                        <FeaturedCaseStudy key={pageCaseStudy.fields.content.sys.id} pageCaseStudy={pageCaseStudy} />
+                    ))}
+                    {button && (
+                        <div className="flex justify-center mt-20">
+                            <ButtonCircle bgColor="ivory" button={button.fields} />
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
