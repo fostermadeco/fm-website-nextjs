ca4f6e9a5ff653ac5d403c37cf692997927c671e
bryansyllaFM <bryan.sylla@fostermade.co>
2022-03-22 11:39:23 -0400

    add opacity, change layout and refactor clientLogo component

--- components/renderer/ClientLogo.tsx
+++ components/renderer/ClientLogo.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { TypeLogoFields } from '@types';
+import { TypeLogo } from '@types';
 import Image from 'next/image';
 
 type ClientLogoPropTypes = {
-    fields: TypeLogoFields;
+    logo: TypeLogo;
 };
 const ClientLogo = (props: ClientLogoPropTypes) => {
-    const { fields } = props;
+    const { logo } = props;
+    const { fields } = logo;
     if (!fields) return null;
-    const { logo } = fields;
-
-    const { fields: logoFields } = logo;
-
-    const { file } = logoFields;
+    const { logo: logoMedia } = fields;
+    const { fields: logoMediaFields } = logoMedia;
+    const { file } = logoMediaFields;
 
     return (
-        <div className="relative w-1/2 h-16 md:w-1/4">
+        <div className="relative w-1/2 h-16 md:w-1/5">
             <Image
                 src={`https:${file.url}`}
                 layout="fill"
                 objectFit="contain"
                 objectPosition="center"
-                className="grayscale"
+                className="grayscale opacity-50"
             />
         </div>
     );
--- components/renderer/ClientLogoGrid.tsx
+++ components/renderer/ClientLogoGrid.tsx
@@ -13,9 +13,9 @@ const ClientLogoGrid = (props: ClientLogoGridsPropTypes) => {
     if (!fields) return null;
     return (
         <div className="container py-10 px-10">
-            <div className="relative flex flex-auto flex-wrap justify-center gap-4">
+            <div className="relative flex flex-auto flex-wrap justify-center gap-8">
                 {logos?.map((logo) => (
-                    <ClientLogo fields={logo.fields} key={logo.sys.id} />
+                    <ClientLogo logo={logo} key={logo.sys.id} />
                 ))}
             </div>
         </div>
